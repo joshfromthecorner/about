bef952deb557b517236cc60f5343feea110b9ea9
Joshua <joshua@Mac-van-Joshua.local>
2026-01-19 21:13:03 +0100

    Fix build error: add background color to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
