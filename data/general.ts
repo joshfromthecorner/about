35b975410e2912650679a81beae559a036eae46e
Joshua <joshua@Mac-van-Joshua.local>
2026-01-20 11:37:34 +0100

    Update about text: change experimentation to experiment design and applied AI tools to Gen AI tools

--- data/general.ts
+++ data/general.ts
@@ -3,7 +3,7 @@ export const generalData = {
   avatar: "/avatar.png",
   jobTitle: "Product/UX Designer at Valsplat",
   about:
-    "I help teams create meaningful products through research-driven design. Previously, I've worked with organizations such as OVpay, NVM, BNG Bank, PostNL, the Dutch Ministry of Health, Kiesraad, Albert Heijn, and Air France–KLM.\n\nMy background spans product and UX design, design sprints, concept development, experimentation, user testing, and applied AI tools. I enjoy tackling diverse design challenges and bringing ideas to life whether that's solving complex problems for clients or building my own furniture at home.\n\nI live in Roelofarensveen with my girlfriend and our British Shorthair cat. Outside of work, I'm a big fan of Lord of the Rings, Star Wars, LEGO, and try to go skiing every year. Most weekends are spent on or next to the football pitch with my friends, father and brother.",
+    "I help teams create meaningful products through research-driven design. Previously, I've worked with organizations such as OVpay, NVM, BNG Bank, PostNL, the Dutch Ministry of Health, Kiesraad, Albert Heijn, and Air France–KLM.\n\nMy background spans product and UX design, design sprints, concept development, experiment design, user testing and Gen AI tools. I enjoy tackling diverse design challenges and bringing ideas to life whether that's solving complex problems for clients or building my own furniture at home.\n\nI live in Roelofarensveen with my girlfriend and our British Shorthair cat. Outside of work, I'm a big fan of Lord of the Rings, Star Wars, LEGO, and try to go skiing every year. Most weekends are spent on or next to the football pitch with my friends, father and brother.",
   contacts: [
     {
       label: "Email",
